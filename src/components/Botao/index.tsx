3f036ee827a68f31333f57aa78fdf2b0b8f26137
Gustavo Mautone Freitas <gustavo.freitas@intelbras.com.br>
2023-01-19 22:58:00 -0300

    Adding children props and new components

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-function Botao() {
+function Botao({ children }: { children: string }) {
   return (
     <button className={style.botao}>
-      Botão
+      { children }
     </button>
   );
 }
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -29,7 +29,7 @@ function Formulario() {
         />
       </div>
 
-      <Botao/>
+      <Botao>Adicionar</Botao>
     </form>
   );
 }
